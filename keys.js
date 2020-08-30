a7c8ad23976f5ea37322256018c1c1ccf6dda1b0
Meghna Nandy <62709400+meghnanandy@users.noreply.github.com>
2020-08-30 10:50:11 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://admin-meghna:adM123@cluster0.znt8y.mongodb.net/shopping_website?retryWrites=true&w=majority'
+    // enter the MongoURI
+    //example: 
+    //MongoURI: 'mongodb+srv://admin-aria:adMin@cluster0.znt8y.mongodb.net/shopping_website?retryWrites=true&w=majority'
 }
